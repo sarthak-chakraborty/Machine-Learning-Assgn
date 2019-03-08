8018fff0363eae883a95df478432794d9f50e476
Sarthak Chakraborty <sarthak.chakraborty@gmail.com>
2019-03-08 16:36:40 +0530

    Graphviz added

--- Assignment2/Part1_sklearn.py
+++ Assignment2/Part1_sklearn.py
@@ -22,22 +22,20 @@ del feature[-1]
 for i in range(len(df1)):
 	row = []
 	for data in df1.iloc[i,:]:
-		row.append(str(data))
-		# if(type(data) == unicode):
-		# 	row.append(str(data))
-		# else:
-		# 	row.append(data)
+		if(type(data) == unicode):
+			row.append(str(data))
+		else:
+			row.append(data)
 	train_data.append(row)
 
 
 for i in range(len(df2)):
 	row = []
 	for data in df2.iloc[i,:]:
-		row.append(str(data))
-		# if(type(data) == unicode):
-		# 	row.append(str(data))
-		# else:
-		# 	row.append(data)
+		if(type(data) == unicode):
+			row.append(str(data))
+		else:
+			row.append(data)
 	test_data.append(row)
 
 
@@ -46,32 +44,66 @@ Y_train = []
 X_test = []
 Y_test = []
 
-X_train, Y_train = [train_data[i][0:-1] for i in range(len(train_data))], [train_data[i][-1] for i in range(len(train_data))]
-X_test, Y_test = [test_data[i][0:-1] for i in range(len(test_data))], [test_data[i][-1] for i in range(len(test_data))]
 
-print(X_train)
-print(Y_train)
+for i in range(0, len(train_data)):
+	row = []
+	for j in range(0,len(train_data[i])):
+		if(j==len(train_data[i])-1):
+			if(train_data[i][j]=='yes'):
+				Y_train.append(1)
+			else:
+				Y_train.append(0)
+		else:
+			if(train_data[i][j]=='low'):
+				row.append(0)
+			elif(train_data[i][j]=='med'):
+				row.append(1)
+			elif(train_data[i][j]=='high'):
+				row.append(2)
+			elif(train_data[i][j]=='yes'):
+				row.append(1)
+			elif(train_data[i][j]=='no'):
+				row.append(0)
+			else:
+				row.append(int(train_data[i][j]))
+	X_train.append(row)
 
-enc = OneHotEncoder(handle_unknown='ignore')
-enc.fit(X_train)
-a = enc.transform(X_train).toarray()
-print(a)
-b = enc.transform(X_test).toarray()
+for i in range(0, len(test_data)):
+	row = []
+	for j in range(0,len(test_data[i])):
+		if(j==len(test_data[i])-1):
+			if(test_data[i][j]=='yes'):
+				Y_test.append(1)
+			else:
+				Y_test.append(0)
+		else:
+			if(test_data[i][j]=='low'):
+				row.append(0)
+			elif(test_data[i][j]=='med'):
+				row.append(1)
+			elif(test_data[i][j]=='high'):
+				row.append(2)
+			elif(test_data[i][j]=='yes'):
+				row.append(1)
+			elif(test_data[i][j]=='no'):
+				row.append(0)
+			else:
+				row.append(int(test_data[i][j]))
+	X_test.append(row)
 
 
-clf1 = DecisionTreeClassifier(random_state=0).fit(a, Y_train)
-label_gini = clf1.predict(b)
-acc_gini = clf1.score(b, Y_test)
-print(acc_gini)
 
-clf2 = DecisionTreeClassifier(criterion='entropy',random_state=0).fit(a,Y_train)
-label_entropy = clf2.predict(b)
-acc_entropy = clf1.score(b, Y_test)
-print(acc_entropy)
+clf1 = DecisionTreeClassifier(random_state=0).fit(X_train, Y_train)
+label_gini = clf1.predict(X_test)
+acc_gini = clf1.score(X_test, Y_test)
 
-print(clf1.decision_path(b))
+clf2 = DecisionTreeClassifier(criterion='entropy',random_state=0).fit(X_train,Y_train)
+label_entropy = clf2.predict(X_test)
+acc_entropy = clf1.score(X_test, Y_test)
+
+print(clf1.decision_path(X_test))
 print("")
-print(clf2.decision_path(b))
+print(clf2.decision_path(X_test))
 
 n_nodes = clf1.tree_.node_count
 children_left = clf1.tree_.children_left
@@ -85,10 +117,8 @@ print(features)
 print(children_right)
 print(threshold)
 
-
-
 import graphviz 
-dot_data = tree.export_graphviz(clf1, out_file=None, class_names=['yes','no'])
+dot_data = tree.export_graphviz(clf1, out_file=None, feature_names=feature,class_names=['yes','no'])
 graph = graphviz.Source(dot_data) 
 
 graph.render("Gini")
@@ -109,7 +139,7 @@ print(threshold)
 
 
 
-dot_data = tree.export_graphviz(clf2, out_file=None, class_names=['yes','no'])
+dot_data = tree.export_graphviz(clf2, out_file=None, feature_names=feature,class_names=['yes','no'])
 graph = graphviz.Source(dot_data) 
 
 graph.render("Entropy")
