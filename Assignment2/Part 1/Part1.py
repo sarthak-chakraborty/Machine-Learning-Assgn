1f150218df9392f406ddf9d18f4fbbd1bb76d531
Sarthak Chakraborty <sarthak.chakraborty@gmail.com>
2019-03-09 11:06:49 +0530

    Add files via upload

--- Assignment2/Part 1/Part1.py
+++ Assignment2/Part 1/Part1.py
@@ -92,12 +92,12 @@ for i in range(0, len(test_data)):
 class DecisionTree:
 	def __init__(self, criteria):
 		self.criteria = criteria
-		self.children_left = []
+		self.children = []
 		self.children_right = []
 		self.features = []
 		self.gini = []
 		self.info_gain = []
-		self.node = 0
+		self.node = 1
 
 	def compute_gini(self, X):
 		length = len(X)
@@ -121,15 +121,16 @@ class DecisionTree:
 		elif(self.criteria == 'entropy'):
 			compute_info_gain(Y)
 
-		if(gini = 0.0):
-			self.feature.append(-1)
+		if(gini == 0.0):
+			self.features.append(-1)
+			self.children.append(-1)
+			print(self.features)
+			print(self.children)
 			return
 
 		measure_feature = []
 		for i in range(n_features):
-			print(i)
 			feat = np.unique(zip(*X)[i])
-			print(feat)
 			measure = []
 			ni = []
 			for l in feat:
@@ -141,10 +142,35 @@ class DecisionTree:
 				ni.append(len(Y_new))
 			measure_feature.append(self.combine_gini(measure, ni, len(X)))
 
-		attribute = index(min(measure_feature))
-		self.feature.append(attribute)
-		
-		del X[attribute]
+		attribute = measure_feature.index(min(measure_feature))
+		self.features.append(attribute)
+		dic = {}
+		for l in np.unique(zip(*X)[attribute]):
+			dic[l] = self.node
+			self.node +=1
+		self.children.append(dic)
+
+		print("Attribute chosen: "+str(attribute))
+		print(self.features)
+		print(measure_feature)
+		print(self.children)
+
+		for l in np.unique(zip(*X)[attribute]):
+			X_new = []
+			Y_new = []
+			for i in range(len(X)):
+				if(X[i][attribute] == l):
+					X_new.append(X[i])
+					Y_new.append(Y[i])
+				
+			print("X_new" + str(X_new))
+			print("Y_new" + str(Y_new))
+			print("")
+			self.fit(X_new, Y_new)
+
+
+
+		# del X[attribute]
 
 			
 
